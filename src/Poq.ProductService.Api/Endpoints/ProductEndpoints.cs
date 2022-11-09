8425a1fd1ef080ac169d0ee5a6edfc4e81708921
Hulkstance <33224919+Hulkstance@users.noreply.github.com>
2022-11-09 09:07:11 +0200

    Remove MockyClient

--- src/Poq.ProductService.Api/Endpoints/ProductEndpoints.cs
+++ src/Poq.ProductService.Api/Endpoints/ProductEndpoints.cs
@@ -8,10 +8,15 @@ namespace Poq.ProductService.Api.Endpoints;
 
 public static class ProductEndpoints
 {
+    private const string ContentType = "application/json";
+    private const string Tag = "Products";
+
     public static void MapProductEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapGet("api/v1/product", GetProducts)
             .WithName("GetProducts")
+            .Produces<Response>(200, ContentType).Produces(404)
+            .WithTags(Tag)
             .CacheOutput(x => x
                 .Expire(TimeSpan.FromSeconds(30))
                 .Tag("cache"));
@@ -26,12 +31,7 @@ public static class ProductEndpoints
     /// <param name="highlight">The keywords for the highlighting, e.g. <code>blue,green</code>.</param>
     /// <param name="mediator">The <see cref="IMediator"/> instance.</param>
     /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
-    /// <response code="200">Returns the subset of products.</response>
-    /// <response code="404">If the result is null.</response>
     /// <returns></returns>
-    [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
-    [Produces("application/json")]
     private static async Task<IResult> GetProducts(
         [FromQuery] double? minPrice,
         [FromQuery] double? maxPrice,
@@ -44,6 +44,6 @@ public static class ProductEndpoints
         var response = await mediator.Send(query, cancellationToken);
         return response.Success
             ? Results.Ok(response)
-            : Results.BadRequest();
+            : Results.NotFound();
     }
 }
